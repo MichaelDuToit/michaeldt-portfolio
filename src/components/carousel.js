c47461530e86c9aae1fc9079a5d5edc8eadadb7b
Michael du Toit <michael.dtoit@gmail.com>
2024-11-11 21:26:32 +1300

    Refactor the Carousel into a simpler carousel without a modal.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -2,11 +2,6 @@ import { useState } from "react";
 
 export default function Carousel({ images })
 {
-
-    const [ modalVisible, setModalVisbility] = useState(false);
-    const [ modalImageIndex, setModalImageIndex ] = useState(0);
-    const [ selectedModalImage, setSelectedModalImage ] = useState("");
-
     const imgArray = images.map((img, index) => ({
         index: index,
         url: img
@@ -14,9 +9,12 @@ export default function Carousel({ images })
 
     const hideArrows = (imgArray.length == 1);
 
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [currentCarouselImage, setCurrentCarouselImage] = useState(imgArray[0]);
+
     function changeSlide(direction)
     {
-        let nextImageIndex = modalImageIndex - direction;
+        let nextImageIndex = currentImageIndex - direction;
         const maxImageIndex = imgArray.length - 1;
 
         if(nextImageIndex < 0)
@@ -28,38 +26,15 @@ export default function Carousel({ images })
             nextImageIndex = 0
         }
 
-        setImage(nextImageIndex);     
-    }
-    
-    function setImage(imgIndex)
-    {
-        setModalImageIndex(imgIndex);
-        setSelectedModalImage(`/images/${imgArray[imgIndex].url}`);
-        setModalVisbility(true);
-    }
-
-    function closeModal()
-    {
-        setModalVisbility(false);
+        setCurrentImageIndex(nextImageIndex);
+        setCurrentCarouselImage(imgArray[nextImageIndex]);
     }
 
     return (
         <div className="carousel">
-            <div className="display">
-                {
-                    imgArray.map(i => (
-                        <img key={i.index} src={`/images/${i.url}`} onClick={() => setImage(i.index)}/>
-                    ))
-                }
-            </div>
-            <div className={`modal ${modalVisible ? "modalVisible" : "" }`}>
-                <div className="modalContent">
-                    <a className="modalClose" onClick={() => closeModal()}>X</a>
-                    <a className={`modalPrev ${hideArrows ? "btnHidden" : ""}`} onClick={() => changeSlide(-1)}>&#10094;</a>
-                    <img src={selectedModalImage} />
-                    <a className={`modalNext ${hideArrows ? "btnHidden" : ""}`} onClick={() => changeSlide(1)}>&#10095;</a>
-                </div>
-            </div>
+            <a className={`modalPrev ${hideArrows ? "btnHidden" : ""}`} onClick={() => changeSlide(-1)}>&#10094;</a>
+            <img src={`/images/${currentCarouselImage.url}`} />
+            <a className={`modalNext ${hideArrows ? "btnHidden" : ""}`} onClick={() => changeSlide(1)}>&#10095;</a>
         </div>
     )
 }
