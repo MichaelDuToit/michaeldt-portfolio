7f1a0c0297ec94419b3757ec01ae602915941cfd
Michael du Toit <michael.dtoit@gmail.com>
2024-11-29 19:37:54 +1300

    Get the carousel hydrating on page load + carousel styling fixes.

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -3,22 +3,28 @@ import { getProjectData, projectsContentPaths } from "@/configuration/markdown-u
 import { MDXRemote } from "next-mdx-remote";
 import {serialize} from 'next-mdx-remote/serialize';
 import Head from 'next/head'
+import { useEffect, useState } from "react";
 import AliceCarousel from 'react-alice-carousel';
 
 export default function Project({ content, bodyContent })
 {
-    const handleDragStart = (e) => e.preventDefault();
-
-    const imgArray = content.images.map((img) => (
-        <img key={img} src={`/images/${img}`} onDragStart={handleDragStart} className="item-inner" role="presentation" />
-    ));
+    const [initialIndex, setInitialIndex] = useState(null);
 
     const config = {
         0: {
             items: 1,
-            itemsFit: 'fill'
+            itemsFit: 'contain'
         }
     }
+
+    const imgArray = content.images.map((img) => (
+        <img key={img} src={`/images/${img}`} onDragStart={(e) => e.preventDefault()} className="item-inner" role="presentation" />
+    ));
+
+    // using an effect to cause the carousel to be hydrated once the page is loaded.
+    useEffect(() => {
+        setInitialIndex(0);
+    });
 
     return (
         <>
@@ -26,17 +32,16 @@ export default function Project({ content, bodyContent })
             <title>{ GeneratePageTitle(content.title) }</title>
             <meta name="description" content={`${content.description}`} />
             </Head>
-            <div className="projectContent viewSection">
-                <AliceCarousel 
-                    activeIndex={0}
+            <AliceCarousel 
+                    activeIndex={initialIndex}
                     mouseTracking 
                     disableButtonsControls
                     autoHeight
                     items={imgArray} 
                     controlsStrategy="alternate"
                     responsive={config}
-                    onInitialized={(e) => e.activeIndex = 0}
                 />
+            <div className="projectContent viewSection">
                 <h3>{content.title}</h3>
                 <MDXRemote {...bodyContent} />
             </div>
