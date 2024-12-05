62e0755c99438e51434a2c696c1504fdd600b4c5
Michael du Toit <michael.dtoit@gmail.com>
2024-12-05 20:15:56 +1300

    Add favicons and webmanifest

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,10 @@ export default function Home({ projects }) {
         <meta name="description" content="Michael du Toit - Full Stack Software Developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <div>
         <div className="landingSplash splashAnimation">
